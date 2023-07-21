8f782c867379c19a8258527ec0b515eb0bec8013
Renzo Reyes <renzoreyes1107@gmail.com>
2023-07-20 19:12:37 -0500

    Update de proyecto funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import Aside from "./layout/Aside";
+import Registros from "./pages/Registros";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+
+      <div className="contenedor">
+        <Aside/>
+      
+    
+      <div className="contenedor-sub">
+        <Routes>
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path="/" element={<Registros />} />
+          <Route exact path="/edituser/:id" element={<EditUser />} />
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+        </Routes>
+        </div>
+      </div>
+
+    </Router>
+  </div>
+);
 }
 
 export default App;
