bfb7fc10eeb0adf9f899be0f1e3d158d5ca50c0f
renzo reyes <renzoreyes1107@gmail.com>
2023-07-21 17:24:40 -0500

    Avance modulo de configuracion

--- src/layout/ModuloConfig/layout/ArticleModConfig.js
+++ src/layout/ModuloConfig/layout/ArticleModConfig.js
@@ -2,37 +2,53 @@ import React from "react";
 import '../estilos/imagen.css';
 import '../estilos/contenedor.css'
 import imagenEmpleados from '../imagenes/empleados.png'
+import { Link } from "react-router-dom";
 
 export default function ArticleModConfig() {
     return (
 
         <article>
-            <div className="card">
+            <div className="card componente-container">
 
-                <div className="card conten" style={{ maxWidth: "300px"}}>
-                    <div className="image-container">
-                        <img src={imagenEmpleados} className="img-modconfig"
-                        alt="Empleados"></img>
+                <div className="container conten">
+
+                    <div className="card ">
+                        <Link className="btn card-modConf">
+
+
+                            <div className="image-container">
+                                <img src={imagenEmpleados} className="img-modconfig"
+                                    alt="Empleados"></img>
+                            </div>
+
+                            <div class="card-body">
+                                <span className="texto">Registrar Empleados</span>
+                            </div>
+
+                        </Link>
                     </div>
-                   
-                        <div class="card-body">
-                            <h5 className="card-title">Registrar Empleados</h5>
-                        </div>
-                </div>
 
-            
-                <div className="card conten" style={{ maxWidth: "300px"}}>
-                    <div className="image-container">
-                        <img src={imagenEmpleados} className="img-modconfig"
-                        alt="Empleados"></img>
+
+
+
+                    <div className="card ">
+                        <Link className="btn card-modConf">
+
+
+                            <div className="image-container">
+                                <img src={imagenEmpleados} className="img-modconfig"
+                                    alt="Empleados"></img>
+                            </div>
+
+                            <div class="card-body">
+                                <span className="texto">Asignar Usuario</span>
+                            </div>
+
+                        </Link>
                     </div>
-                   
-                        <div class="card-body">
-                            <h5 className="card-title">Registrar Empleados</h5>
-                        </div>
-                </div>
-            
 
+
+                </div>
             </div>
 
         </article>
